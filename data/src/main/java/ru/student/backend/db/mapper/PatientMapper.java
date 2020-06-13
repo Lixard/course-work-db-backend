b303986a87a7c3a49150234c6e88c14a6a52fc87
Maksim Borisov <lixard870@gmail.com>
2020-06-13 14:43:35 +0300

    Add doctor's patient for current period sql request

--- data/src/main/java/ru/student/backend/db/mapper/PatientMapper.java
+++ data/src/main/java/ru/student/backend/db/mapper/PatientMapper.java
@@ -2,7 +2,9 @@ package ru.student.backend.db.mapper;
 
 import org.apache.ibatis.annotations.*;
 import ru.student.backend.db.model.Patient;
+import ru.student.backend.db.model.PatientsWithAppointmentDate;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
@@ -23,6 +25,39 @@ public interface PatientMapper {
                     "FROM patients"
     )
     List<Patient> getPatients();
+
+    @Select(
+            //language=PostgreSQL
+            "SELECT p.patient_id, " +
+                    "p.last_name," +
+                    "p.first_name, " +
+                    "p.second_name, " +
+                    "p.sex, " +
+                    "p.birthday, " +
+                    "p.policy, " +
+                    "p.serial_passport, " +
+                    "p.number_passport, " +
+                    "a.appointment_date " +
+                    "FROM appointments a " +
+                    "JOIN patients p on a.patient_id = p.patient_id " +
+                    "WHERE a.doctor_id = #{doctorId} " +
+                    "GROUP BY a.doctor_id, " +
+                    "p.patient_id, " +
+                    "p.last_name, " +
+                    "p.second_name, " +
+                    "p.sex, " +
+                    "p.birthday, " +
+                    "p.policy, " +
+                    "p.serial_passport, " +
+                    "p.number_passport, " +
+                    " a.appointment_date " +
+                    "HAVING min(a.appointment_date) > #{dateStart} " +
+                    "AND max(a.appointment_date) < #{dateEnd}"
+
+    )
+    List<PatientsWithAppointmentDate> getPatientsForCurrentDoctorForPeriod(@Param("doctorId") int doctorId,
+                                                                           @Param("dateStart") LocalDateTime dateStart,
+                                                                           @Param("dateEnd") LocalDateTime dateEnd);
 
     @Select(
             //language=PostgreSQL
